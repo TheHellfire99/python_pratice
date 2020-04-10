b4b40e78e63c28087c146c24dab546a3007f608c
Shaurya Srivastav <Shaurya0904@gmail.com>
2020-04-10 15:58:05 -0700

    here is some of my new practice file some previous ones got deleted

---
+++ practice1.py
@@ -0,0 +1,16 @@
+#ask for age 
+age = input("how old are you: ")
+
+if age:
+    #18-21 you wear a wrist band 
+    if int(age) >= 18 and int(age) < 21:
+        print("you can enter but you need a wrist band")
+    #21+ drink, normal entry 
+    elif int(age) >= 21:
+        print("you are good to enter and can drink")
+    #too young, sorry
+    else: 
+        print("you are too young buddy!")
+else: 
+    print("please enter an age")
+
