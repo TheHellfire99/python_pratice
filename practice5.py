0cdb8e5741f4c77c0da6b77f3a4fc4b8858ce40c
Shaurya Srivastav <Shaurya0904@gmail.com>
2020-04-12 15:23:37 -0700

    practice 5 has been added and also https://www.youtube.com/watch?v=dQw4w9WgXcQ

---
+++ practice5.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+user = input("eyy baby how you doing?: ")
+
+while user != "Stop":
+    print(user)
+    user = input()
+
+webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+
+
